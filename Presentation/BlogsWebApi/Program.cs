565a71c5b7696bd75b861e306f2f588d69ba8099
MVMmaksM <vaganov.maksim94@yandex.ru>
2024-05-26 20:54:34 +0500

    create documentation swagger

--- Presentation/BlogsWebApi/Program.cs
+++ Presentation/BlogsWebApi/Program.cs
@@ -1,3 +1,5 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c=>
+{
+    c.IncludeXmlComments(string.Format(@"{0}Blogs.xml", AppDomain.CurrentDomain.BaseDirectory));
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Blogs"
+    });
+});
 
 var app = builder.Build();
 
@@ -13,7 +23,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c=>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json","Blogs");
+    });
 }
 
 app.UseHttpsRedirection();
