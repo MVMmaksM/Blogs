57ce9f95b6d77d8531fb04b51e9697e71374e233
MVMmaksM <vaganov.maksim94@yandex.ru>
2024-05-27 16:02:41 +0500

    create iuserrepository

---
+++ Core/Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> Create(User user);
+        Task<User> GetById(int id);
+        Task<bool> IsAccessCredential(User user);
+    }
+}
