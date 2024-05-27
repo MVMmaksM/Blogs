d586381022861c0f7fbe9a59a2ab0e05baa563f2
MVMmaksM <vaganov.maksim94@yandex.ru>
2024-05-27 15:43:23 +0500

    create articlerepository

---
+++ Core/Application/Interfaces/IArticleRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IArticleRepository
+    {
+        Task<Article> Create(Article article);
+        Task<Article> Update(Article article);
+        Task<Article> GetArticle(int id);
+        Task<int> Delete(int id);
+        Task<List<Article>> GetArticles();
+    }
+}
