b73fb8c273169d3323babb37dd271df7b7711445
MVMmaksM <vaganov.maksim94@yandex.ru>
2024-05-27 16:18:19 +0500

    create articleresponce

---
+++ Core/Application/Services/ArticleService.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ArticleService
+    {
+        private readonly IArticleRepository _articleRepository;
+        public ArticleService(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
+
+        public async Task<Article> GetArticleById(int id) 
+        {
+            return await _articleRepository.GetById(id);
+        }
+    }
+}
---
+++ Presentation/BlogsWebApi/Contracts/Responce/ArticleResponce.cs
@@ -0,0 +1,14 @@
+﻿namespace BlogsWebApi.Contracts.Responce
+{
+    public class ArticleResponce
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DateCreate { get; set; }
+        public DateTime? DateUpdate { get; set; }
+        public string Text { get; set; }
+        public int CountView { get; set; }
+        public int CountLike { get; set; }
+        public string Author { get; set; }
+    }
+}
