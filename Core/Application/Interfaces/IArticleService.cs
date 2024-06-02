00beea9dcfca943bf83864711aca3c256f5e686b
MVMmaksM <vaganov.maksim94@yandex.ru>
2024-06-02 18:01:26 +0500

    add iarticleservice

---
+++ Core/Application/Interfaces/IArticleService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IArticleService
+    {
+        Task<Article> GetArticleById(int id);
+        Task<List<Article>> GetArticles();
+        Task<Article> CreateArticle(Article article);
+        Task<Article> UpdateArticle(Article article);
+        Task<int> DeletArticle(int id);
+    }
+}
