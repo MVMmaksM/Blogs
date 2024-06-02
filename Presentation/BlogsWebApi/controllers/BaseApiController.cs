cbdb334e2cbe13f76af2e607868511897e9e485b
MVMmaksM <vaganov.maksim94@yandex.ru>
2024-06-02 11:52:45 +0500

    create basecontroller and artile controller

---
+++ Presentation/BlogsWebApi/controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogsWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public abstract class BaseApiController : ControllerBase
+    {
+
+    }
+}
---
+++ Presentation/BlogsWebApi/controllers/v1/ArticleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Application.Services;
+
+namespace BlogsWebApi.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class ArticleController : BaseApiController
+    {
+        private readonly ArticleService _articleService;
+
+        public ArticleController(ArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll() 
+        {
+            return Ok(await _articleService.GetArticles());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetArticle(int id) 
+        {
+            return Ok(await _articleService.GetArticleById(id));
+        }
+    }
+}
